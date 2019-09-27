b3c70b545e977b720f3a8a465e2496de99bae702
Yandisa Madinga <mdnyan002@sl-dual-213.cs.uct.ac.za>
2019-09-27 10:41:42 +0200

    Working simulation

---
+++ src/WordPanel.java
@@ -0,0 +1,57 @@
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.concurrent.CountDownLatch;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+public class WordPanel extends JPanel implements Runnable {
+		public static volatile boolean done;
+		private WordRecord[] words;
+		private int noWords;
+		private int maxY;
+		private int y_dim = 20;//(int)Math.random()*(600-5)+5;
+		
+		public void paintComponent(Graphics g) {
+		    int width = getWidth();
+		    int height = getHeight();
+		    g.clearRect(0,0,width,height);
+		    g.setColor(Color.red);
+		    g.fillRect(0,maxY-10,width,height);
+
+		    g.setColor(Color.black);
+		    g.setFont(new Font("Helvetica", Font.PLAIN, 26));
+		   //draw the words
+		   //animation must be added 
+		    for (int i=0;i<noWords;i++){	    	
+		    	//g.drawString(words[i].getWord(),words[i].getX(),words[i].getY());	
+		    	g.drawString(words[i].getWord(),words[i].getX(),y_dim);  //y-offset for skeleton so that you can see the words	
+		        
+		    }
+		   
+		  }
+		
+		WordPanel(WordRecord[] words, int maxY) {
+			this.words=words; //will this work?
+			noWords = words.length;
+			done=false;
+			this.maxY=maxY;		
+		}
+		
+		public void run() {
+			System.out.println("..............");
+			try{Thread.sleep(100);y_dim+=5;}catch(Exception e) {}
+			if(y_dim>500) {
+				y_dim =20 ;
+			}
+			repaint();
+			
+		}
+
+	}
+
+
